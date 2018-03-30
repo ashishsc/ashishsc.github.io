320ec36a8dd926e959f8fcf3f37982c4d45cf342
Ashish Chandwani <ashish.s.chandwani@gmail.com>
2018-03-30 14:43:23 -0400

    Start some basic layout

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,22 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
-import Style
-import Style.StyleSheet
-
-
----- MODEL ----
+import Color
+import Element exposing (..)
+import Element.Attributes exposing (..)
+import Html
+import Style exposing (..)
+import Style.Color as Color
+import Style.Font as Font
+import Style.Transition as Transition
 
 
 type alias Model =
-    {}
+    ()
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
+    ( (), Cmd.none )
 
 
 type Msg
@@ -31,25 +28,55 @@ update msg model =
     ( model, Cmd.none )
 
 
-stylesheet : Style.StyleSheet
+type Styles
+    = None
+    | Heading
+    | Nav
+    | NavLink
+
+
+defaultFont : List Font
+defaultFont =
+    [ Font.font "Open Sans"
+    , Font.font "helvetica"
+    , Font.font "arial"
+    , Font.font "sans-serif"
+    ]
+
+
+stylesheet : StyleSheet Styles variation
 stylesheet =
-    undefined
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Ashish's stuff" ]
+    Style.styleSheet
+        [ style None []
+        , style Heading
+            [ Font.typeface defaultFont
+            , Font.size 60
+            ]
+        , style Nav
+            [ Font.size 16
+            , Font.typeface defaultFont
+            ]
+        , style NavLink
+            []
         ]
 
 
-
----- PROGRAM ----
+view : Model -> Html.Html Msg
+view model =
+    Element.layout stylesheet <|
+        column None
+            [ center, width fill, height fill, moveDown 50 ]
+            [ el Heading [ center ] (text "Ashish")
+            , navigation Nav
+                [ spacing 80, moveDown 20 ]
+                { name = "Main Navigation"
+                , options =
+                    [ link "/about" (el NavLink [] (text "About"))
+                    , link "/blog" (el NavLink [] (text "Blog"))
+                    , link "https://github.com/ashishsc" (el NavLink [] (text "Projects"))
+                    ]
+                }
+            ]
 
 
 main : Program Never Model Msg
